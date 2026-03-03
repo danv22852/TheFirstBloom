accbe8fdf25fd06fee3bf680e1c86a359a08ab1d
Alvaro Canseco-Martinez <alvcm1501@gmail.com>
2026-03-03 00:55:02 -0500

    Added barallels to first floor plus some coins

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // This function is called when another collider enters the trigger collider attached to this GameObject
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Destroy the item after it's collision triggered
+        Destroy(gameObject);
+    }
+}
