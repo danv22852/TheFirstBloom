4c6f96ec1947dfd41c2255adeeae8b2c8d8c8746
Daniel Rojas <danv22852@gmail.com>
2026-03-03 17:04:54 -0500

    Revert "Reapply "Added Swinging in Overworld + Alien Pickup""
    This reverts commit 1739a27b5888ce38b694886163388778fe9bd7d3.

--- Assets/Scripts/DamageTest.cs
+++ Assets/Scripts/DamageTest.cs
@@ -3,17 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class DamageTest : MonoBehaviour
 {   private void OnTriggerEnter2D(Collider2D other)
-{
-    if (other.CompareTag("Player"))
     {
-        if (GameManager.Instance == null)
+        if (other.CompareTag("Player"))
         {
-            Debug.LogError("GameManager is NULL!");
-            return;
+            GameManager.Instance.TakeDamage(1);
         }
-
-        GameManager.Instance.TakeDamage(1);
     }
 }
-}
-
